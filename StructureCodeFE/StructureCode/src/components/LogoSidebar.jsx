57cf84b96bf61e71d2efce91cf919091a867bbdd
nampham194205-cmd <nampham194205@gmail.com>
2025-09-29 13:29:08 +0700

    OEM-19

---
+++ StructureCodeFE/StructureCode/src/components/LogoSidebar.jsx
@@ -0,0 +1,19 @@
+export default function LogoSidebar() {
+  return (
+    <a href="index-2.html" className="navbar-brand mx-4 mb-3">
+      <h4 className="text-primary">
+        <img
+          src="../image/logo.jpg"
+          alt=""
+          style={{
+            width: "60px",
+            height: "60px",
+            marginRight: "10px",
+            borderRadius: "50%",
+          }}
+        />
+        MEGA TEMA
+      </h4>
+    </a>
+  );
+}
