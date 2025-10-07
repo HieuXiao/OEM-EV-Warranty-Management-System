c7c98da501074eaa6aab0e644c2db20f3c8e4052
MrHieu <hieunm.se@gmail.com>
2025-10-08 04:30:36 +0700

    add evm path

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/admin/users" element={<AdminUserManagement />} />
+        <Route path="/evmstaff" element={< EVMStaff />} />
       </Routes>
     </>
   );
