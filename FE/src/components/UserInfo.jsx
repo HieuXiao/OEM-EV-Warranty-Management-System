b2134fe8d301f85f1d2f982b7b2772793e7abff4
iMax <nampham194205@gmail.com>
2025-11-22 13:47:49 +0700

    Remove Avatar component from UserInfo
    Removed Avatar component from UserInfo.

--- FE/src/components/UserInfo.jsx
+++ FE/src/components/UserInfo.jsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import { cn } from "@/lib/utils";
 import useAuth from "@/hook/useAuth";
 
@@ -8,10 +8,6 @@ export default function UserInfo() {
   return (
     <div className="p-4 border-b border-border">
       <div className="flex items-center gap-3">
-        <Avatar className="h-10 w-10">
-          <AvatarImage />
-          <AvatarFallback className={cn("text-white")}></AvatarFallback>
-        </Avatar>
         <div className="flex-1 min-w-0">
           <p className="text-sm font-medium text-foreground truncate">
             {auth.fullName} {/* {user.name} */}
